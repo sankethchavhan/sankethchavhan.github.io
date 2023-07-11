986d0a8770d9594962c6b800bd95de8814aa9ff0
Sriraksha M S <102527744+SrirakshaMS@users.noreply.github.com>
2023-07-11 22:39:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top => offset && top < offset + height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -32,10 +32,10 @@ window.onscroll = () => {
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-}
+};
 
 ScrollReveal({
-    reset: true,
+    // reset: true,
     distance: '80px',
     duration: 800,
     delay: 200
@@ -54,3 +54,48 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+
+function copyToClipboard(text,) {
+    navigator.clipboard.writeText(text)
+        .then(function() {
+            console.log("Text copied to clipboard");
+        })
+        .catch(function(error) {
+            console.error("Failed to copy text: ", error);
+        });
+        
+        // alert("Link Copied!");
+}
+
+// function changeIcon(anchor){
+//     var icon = anchor.querySelector("i");
+//     icon.classList.toggle('bx-check');
+//     icon.classList.toggle('bx-link');
+// }
+
+function changeIcon(anchor){
+    var icon = anchor.querySelector("i");
+    if(icon.className=="bx bx-link"){
+      icon.className = "bx bx-check";
+    }
+    else if(icon.className=="bx bx-check"){
+      icon.className = "bx bx-check";
+    }
+}
+
+
+// function refresh(){
+//     window.location.href = "..index.html";
+// }
+
+var currentURL = window.location.href;
+
+// Set the homepage URL
+var homepageURL = "https://sankethchavhan.github.io/"; // Replace with your actual homepage URL
+
+// Check if the current URL is the homepage
+if (currentURL !== homepageURL) {
+  // Redirect to the homepage
+  window.location.href = homepageURL;
+}
